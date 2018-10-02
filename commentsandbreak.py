461da6868739ed60fb51008449e5670ceb851f36
dr3as <dr3as@dr3as.net>
2018-10-02 21:31:22 +0200

    First version1

---
+++ commentsandbreak.py
@@ -0,0 +1,12 @@
+magicNumber = 26
+
+# comment
+'''dette
+er kommentar'''
+
+for n in range(101):
+    if n is magicNumber:
+        print(n, "is the magic number.")
+        break
+    else:
+        print(n)
